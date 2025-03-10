00d6389bf02b010c7de0ddf178bda91539003765
Gerfy1 <gerfynlol@gmail.com>
2025-03-10 13:26:48 -0300

    add cors update 2

--- src/main/java/com/Jobs/JobsMemory/config/WebConfig.java
+++ src/main/java/com/Jobs/JobsMemory/config/WebConfig.java
@@ -10,6 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins(new String[]{"https://login-angular-eight.vercel.app/login"}).allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE"}).allowedHeaders(new String[]{"*"}).allowCredentials(true);
+        registry.addMapping("/**").allowedOrigins(new String[]{"https://login-angular-eight.vercel.app"}).allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE"}).allowedHeaders(new String[]{"*"}).allowCredentials(true);
     }
 }
